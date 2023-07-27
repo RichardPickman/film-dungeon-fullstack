f53c630a0ef4b0870864a49847403070abf97e56
RichardPickman <drezzerock@gmail.com>
2023-07-27 13:29:25 +0200

    cards component

---
+++ frontend/src/components/GameCard/GameCardSkeleton.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { Skeleton } from "../ui/skeleton";
+
+export const GameCardSkeleton = () => {
+  return (
+    <div className="flex p-4 border justify-between items-center rounded">
+      <Skeleton className="h-6 w-[250px]" />
+      <div className="flex gap-2">
+        <Skeleton className="h-10 w-36" />
+        <Skeleton className="h-10 w-20" />
+      </div>
+    </div>
+  );
+};
