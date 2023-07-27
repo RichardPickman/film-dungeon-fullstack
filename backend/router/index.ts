4dbff59621ed21d5fba92d40c5f5b30223c76840
RichardPickman <drezzerock@gmail.com>
2023-07-27 13:51:02 +0200

    add forgotten router

--- backend/router/index.ts
+++ backend/router/index.ts
@@ -1,43 +1,8 @@
-import e, { Request, Response } from "express";
-
-
-const createGame = (req: Request, res: Response) => {
-
-};
-
-const getGame = (req: Request, res: Response) => {
-    console.log('get game');
-};
-
-const updateGame = (req: Request, res: Response) => {
-    console.log('update game');
-};
-
-const deleteGame = (req: Request, res: Response) => {
-    console.log('delete game');
-};
+import e from "express";
+import gameRouter from "./game";
 
 const router = e();
 
-const handleGame = (req: Request, res: Response) => {
-    if (req.method === 'GET') {
-        createGame(req, res);
-    }
-
-    if (req.method === 'POST') {
-        getGame(req, res);
-    }
-
-    if (req.method === 'UPDATE') {
-        updateGame(req, res);
-    }
-
-    if (req.method === 'DELETE') {
-        deleteGame(req, res);
-    }
-}
-
-router.use('/games/new', createGame);
-router.use('/games/:id', handleGame);
+router.use("/game", gameRouter);
 
 export default router;
