62a13672cbf79c61a174c82bc2118a2c700af559
RichardPickman <drezzerock@gmail.com>
2023-08-06 14:43:12 +0200

    add reusable card

--- frontend/src/components/Card/index.tsx
+++ frontend/src/components/Card/index.tsx
@@ -1,20 +1,39 @@
 import Image from 'next/image';
+import { Skeleton } from '../ui/skeleton';
+import { UserIcon } from '../Icons/User';
 
 interface Props {
-    image?: string;
+    image?: ImageInfo;
+    isActive?: boolean;
+    onClick: () => void;
 }
 
-export const Card = ({ image = '' }: Props) => {
-    return (
-        <div className="flex justify-center items-centerrounded-lg aspect-square">
-            <div className="relative aspect-square rounded-lg overflow-hidden border bg-slate-800">
-                <Image
-                    className="object-cover"
-                    src={image || '/sadie.png'}
-                    fill
-                    alt="sadie"
-                />
+export const Card = ({ image, onClick }: Props) => (
+    <div
+        className="relative sm:w-10/12 md:w-8/12 lg:w-6/12 aspect-square rounded overflow-hidden border"
+        onClick={onClick}
+    >
+        {!image && (
+            <div className="flex w-full h-full items-center justify-center">
+                <UserIcon className="w-6/12 h-6/12" />
             </div>
+        )}
+        {image && (
+            <Image
+                className="object-cover"
+                src={image.fileUrl}
+                fill
+                alt="Картинка карточки"
+                sizes="(max-width: )"
+            />
+        )}
+    </div>
+);
+
+export const CardSkeleton = () => {
+    return (
+        <div className="relative w-6/12 aspect-square rounded">
+            <Skeleton className="w-full h-full" />
         </div>
     );
 };
