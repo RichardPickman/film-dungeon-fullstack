fb8fb60b4c9c77fd211a474f21dc9320bda77849
RichardPickman <drezzerock@gmail.com>
2023-08-06 14:32:30 +0200

    frontend: update generics

--- frontend/src/actions/fetch/index.ts
+++ frontend/src/actions/fetch/index.ts
@@ -2,41 +2,41 @@ import axios, { AxiosError } from 'axios';
 
 const json = (data: unknown) => JSON.stringify(data);
 
-export const create = async <T>(url: string, values: T) => {
+export const create = async <T, U>(url: string, values: T) => {
     try {
         const { data } = await axios.post(url, values);
 
-        return data as Game;
+        return data as U;
     } catch (e) {
         throw new Error('Error during creating of item');
     }
 };
 
-export const get = async <T>(url: string) => {
+export const get = async <T, U>(url: string) => {
     try {
         const { data } = await axios.get(url).catch(err => err.response);
 
-        return data as T;
+        return data as U;
     } catch (e: unknown) {
         throw new Error('Error during getting an items');
     }
 };
 
-export const update = async (url: string, values: unknown) => {
+export const update = async <T, U>(url: string, values: unknown) => {
     try {
         const { data } = await axios.put(url, values);
 
-        return data as Game;
+        return data as U;
     } catch (e) {
         throw new Error('Error during updating an item');
     }
 };
 
-export const remove = async (url: string) => {
+export const remove = async <T, U>(url: string) => {
     try {
         const { data } = await axios.delete(url);
 
-        return data as Game;
+        return data as T;
     } catch (e) {
         throw new Error('Error during removing an item');
     }
