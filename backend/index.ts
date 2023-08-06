5fb04647da99f9d2fcbeeeee336caad873d98604
RichardPickman <drezzerock@gmail.com>
2023-08-06 14:31:49 +0200

    prettier formatting

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,11 @@
-import express from "express";
-import { createServer } from "http";
-import cors from "cors";
-import bodyParser from "body-parser";
-import router from "./router";
-import { configs } from "./config";
-import { appLogger } from "./middlewares";
-import { errorHandler } from "./middlewares/ErrorHandler";
+import express from 'express';
+import { createServer } from 'http';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import router from './router';
+import { configs } from './config';
+import { appLogger } from './middlewares';
+import { errorHandler } from './middlewares/ErrorHandler';
 
 const { PORT } = configs;
 const app = express();
@@ -14,11 +14,11 @@ const httpServer = createServer(app);
 app.use(bodyParser.json());
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: 'http://localhost:3000',
     })
 );
 app.use(appLogger);
 app.use(router);
-app.use(errorHandler)
+app.use(errorHandler);
 
 httpServer.listen(PORT, () => console.log(`Server started on port ${PORT}`));
--- backend/middlewares/ErrorHandler/index.ts
+++ backend/middlewares/ErrorHandler/index.ts
@@ -1,6 +1,5 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, Response } from 'express';
 
-
-export const errorHandler = (req: Request, res: Response, next: NextFunction) => {
-    res.status(500).json({ message: 'Error' })
-}
+export const errorHandler = (req: Request, res: Response) => {
+    res.status(500).json({ message: 'Error' });
+};
