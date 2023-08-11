9bfde264caad0c4620b5cd9df8ce1faef05d8ef3
RichardPickman <drezzerock@gmail.com>
2023-08-11 18:14:01 +0200

    add websocket server

--- backend/index.ts
+++ backend/index.ts
@@ -7,6 +7,13 @@ import { configs } from './config';
 import { appLogger } from './middlewares';
 import { errorHandler } from './middlewares/ErrorHandler';
 
+import { Server } from 'socket.io';
+import { randomUUID } from 'crypto';
+import { getReadyGame } from './services/game';
+import { db } from './db';
+import { session } from './db/schemas/session';
+import { eq } from 'drizzle-orm';
+
 const { PORT } = configs;
 const app = express();
 const httpServer = createServer(app);
@@ -17,8 +24,85 @@ app.use(
         origin: 'http://localhost:3000',
     })
 );
-app.use(appLogger);
+// app.use(appLogger);
 app.use(router);
 app.use(errorHandler);
 
 httpServer.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+const io = new Server(5005, {
+    cors: {
+        origin: 'http://localhost:3000',
+    },
+});
+
+io.on('connection', socket => {
+    socket.on('create_room', async data => {
+        const { id: gameId, ...sessionData } = data;
+        const id = randomUUID();
+
+        try {
+            const currentGame = await getReadyGame(Number(gameId));
+
+            if (!currentGame) {
+                throw new Error('Game not found');
+            }
+
+            console.log(`Room ${id} created`);
+
+            const state = {
+                ...sessionData,
+                isDungeon: Boolean(currentGame.dungeons[0]),
+                sessionId: id,
+                game: currentGame,
+            };
+
+            const game = await db.insert(session).values(state).returning();
+
+            socket.join(id);
+
+            console.log(`Current game: ${JSON.stringify(game)}`);
+
+            socket.emit('created_room', game[0]);
+        } catch (e) {
+            console.log(e);
+            socket.emit('create_room_error', e);
+        }
+    });
+
+    socket.on('host_change', async data => {
+        try {
+            console.log(`Updating data of game ${data.sessionId}`);
+
+            const { sessionId, ...rest } = data;
+
+            const [game] = await db
+                .update(session)
+                .set(rest)
+                .where(eq(session.sessionId, sessionId))
+                .returning();
+
+            console.log(`Game updated: ${sessionId} ${game}`);
+
+            socket.to(sessionId).emit('state_change', game);
+        } catch (e) {
+            console.log(e);
+            socket.emit('create_room_error', e);
+        }
+    });
+
+    socket.on('join_room', async data => {
+        try {
+            const { sessionId } = data;
+            socket.join(sessionId);
+
+            console.log('Join room: ', sessionId);
+            const game = await db.select().from(session).where(eq(session.sessionId, sessionId));
+
+            socket.emit('state_change', game[0]);
+        } catch (e) {
+            console.log(e);
+            socket.emit('join_room_error', e);
+        }
+    });
+});
