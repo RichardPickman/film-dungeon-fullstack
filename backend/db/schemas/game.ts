c41e0b91800b31afa35a82f3b808631f7a234867
RichardPickman <drezzerock@gmail.com>
2023-08-11 18:12:26 +0200

    update relations

--- backend/db/schemas/game.ts
+++ backend/db/schemas/game.ts
@@ -6,6 +6,10 @@ export const game = pgTable('game', {
     name: varchar('name', { length: 256 }),
 });
 
+export const gameRelations = relations(game, ({ many }) => ({
+    dungeons: many(dungeon),
+}));
+
 export const dungeon = pgTable('dungeon', {
     id: serial('id').primaryKey(),
     image: json('image'),
@@ -13,16 +17,41 @@ export const dungeon = pgTable('dungeon', {
     gameId: integer('game_id')
         .notNull()
         .references(() => game.id, { onDelete: 'cascade' }),
-    bossId: integer('boss_id').references(() => monster.id, { onDelete: 'set null' }),
 });
+
+export const dungeonRelations = relations(dungeon, ({ many, one }) => ({
+    monsters: many(monster),
+    gameId: one(game, { fields: [dungeon.gameId], references: [game.id] }),
+    boss: one(boss, { fields: [dungeon.id], references: [boss.dungeonId] }),
+}));
 
 export const monster = pgTable('monster', {
     id: serial('id').primaryKey(),
     hp: integer('hp'),
     name: varchar('name', { length: 256 }),
     image: json('image'),
-    dungeonId: integer('dungeon_id'),
+    dungeonId: integer('dungeon_id').references(() => dungeon.id, { onDelete: 'cascade' }),
 });
+
+export const boss = pgTable('boss', {
+    id: serial('id').primaryKey(),
+    hp: integer('hp'),
+    name: varchar('name', { length: 256 }),
+    image: json('image'),
+    dungeonId: integer('dungeon_id')
+        .notNull()
+        .references(() => dungeon.id, { onDelete: 'cascade' }),
+});
+
+export const bossRelations = relations(boss, ({ many, one }) => ({
+    questions: many(question),
+    dungeonId: one(dungeon, { fields: [boss.dungeonId], references: [dungeon.id] }),
+}));
+
+export const monsterRelations = relations(monster, ({ many, one }) => ({
+    questions: many(question),
+    dungeonId: one(dungeon, { fields: [monster.dungeonId], references: [dungeon.id] }),
+}));
 
 export const question = pgTable('question', {
     id: serial('id').primaryKey(),
@@ -32,24 +61,11 @@ export const question = pgTable('question', {
     answers: json('answers'),
     letters: json('letters'),
     numbers: json('numbers'),
-    monsterId: integer('monster_id').notNull(),
+    monsterId: integer('monster_id').references(() => monster.id, { onDelete: 'set null' }),
+    bossId: integer('boss_id').references(() => boss.id, { onDelete: 'set null' }),
 });
-
-export const gameRelations = relations(game, ({ many }) => ({
-    dungeons: many(dungeon),
-}));
-
-export const dungeonRelations = relations(dungeon, ({ many, one }) => ({
-    monsters: many(monster),
-    gameId: one(game, { fields: [dungeon.gameId], references: [game.id] }),
-    boss: one(monster, { fields: [dungeon.bossId], references: [monster.id] }),
-}));
-
-export const monsterRelations = relations(monster, ({ many, one }) => ({
-    questions: many(question),
-    dungeonId: one(dungeon, { fields: [monster.dungeonId], references: [dungeon.id] }),
-}));
 
 export const questionRelations = relations(question, ({ one }) => ({
     monster: one(monster, { fields: [question.monsterId], references: [monster.id] }),
+    boss: one(boss, { fields: [question.bossId], references: [boss.id] }),
 }));
