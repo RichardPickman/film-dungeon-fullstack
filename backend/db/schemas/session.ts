25ee607275b07f12927a59ffef9aa3ad235a7c9e
RichardPickman <drezzerock@gmail.com>
2023-08-11 18:12:44 +0200

    add session schema for history and websockets

---
+++ backend/db/schemas/session.ts
@@ -0,0 +1,16 @@
+import { boolean, integer, json, pgTable, serial, varchar } from 'drizzle-orm/pg-core';
+
+export const session = pgTable('session', {
+    id: serial('id').primaryKey(),
+    sessionId: varchar('sessionId').notNull(),
+    game: json('game'),
+    dungeon: json('dungeon'),
+    monster: json('monster'),
+    question: json('question'),
+    isDungeon: boolean('isDungeon'),
+    isMonster: boolean('isMonster'),
+    isQuestion: boolean('isQuestion'),
+    monsterHealth: integer('monsterHealth'),
+    gameHealth: integer('gameHealth'),
+    isImageShowing: boolean('isImageShowing'),
+});
