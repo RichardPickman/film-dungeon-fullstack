3a0c72dd63e6ab7ec4d4263ab37145f343c3645e
RichardPickman <drezzerock@gmail.com>
2023-08-13 20:27:48 +0200

    add differend file types

--- frontend/src/app/api/uploadthing/core.ts
+++ frontend/src/app/api/uploadthing/core.ts
@@ -3,7 +3,11 @@ import { createUploadthing, type FileRouter } from 'uploadthing/next';
 const f = createUploadthing();
 
 export const ourFileRouter = {
-    imageUploader: f({ image: { maxFileSize: '4MB' } })
+    imageUploader: f({
+        image: { maxFileSize: '4MB' },
+        video: { maxFileSize: '16MB' },
+        audio: { maxFileSize: '4MB' },
+    })
         .middleware(() => ({}))
         .onUploadComplete(async ({ file }) => {
             console.log('Upload complete');
