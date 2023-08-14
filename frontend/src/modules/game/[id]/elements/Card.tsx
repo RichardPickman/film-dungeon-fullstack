a94e548fc772bd19b28ae725578b24e0552b5e1a
RichardPickman <drezzerock@gmail.com>
2023-08-14 13:41:24 +0200

    update cards for game

--- frontend/src/modules/game/[id]/elements/Card.tsx
+++ frontend/src/modules/game/[id]/elements/Card.tsx
@@ -2,80 +2,82 @@ import { UserIcon } from '@/components/Icons/User';
 import { Check } from 'lucide-react';
 import Image from 'next/image';
 
+export const CardWithCheck = ({ item }: { item: Monster | Dungeon }) => {
+    return (
+        <>
+            <Image
+                className="object-cover brightness-50"
+                src={item.image!.fileUrl}
+                alt="Картинка карточки"
+                sizes="(max-width:)"
+                fill
+            />
+            <Check className="absolute z-10 bottom-2 right-2 w-6 h-6" />
+        </>
+    );
+};
+
 interface Props {
-    currentItem: Monster | Dungeon | undefined | null;
-    isActive?: boolean;
-    arr: Monster[] | Dungeon[];
+    items: Dungeon[] | Monster[] | null;
+    currentItem: Dungeon | Monster | null;
 }
 
-export const CardsWithStatus = ({ arr, currentItem }: Props) => {
-    if (!currentItem) {
-        console.log('Current item not provided for CardsWithStatus');
+export const Cards = ({ items, currentItem }: Props) => {
+    if (!items) {
+        console.log('No items providede');
 
         return null;
     }
 
-    if (!arr) {
-        console.log('Array not provided for CardsWithStatus');
-
-        return null;
-    }
-
-    const currentIndex = arr.findIndex(item => item.id === currentItem.id);
-
-    return (
-        <div className="flex flex-col w-full h-full gap-2 overflow-auto items-center justify-center">
-            {arr.map((item, index) =>
-                item.image ? (
-                    <div
-                        className="relative w-full md:w-10/12 lg:w-8/12 aspect-square overflow-hidden border rounded"
-                        key={item.id + item.name}
-                    >
-                        {currentIndex > index && (
-                            <>
-                                <Image
-                                    className="object-cover opacity-10"
-                                    src={item.image.fileUrl}
-                                    alt="Картинка карточки"
-                                    sizes="(max-width:)"
-                                    fill
-                                />
-                                <Check className="absolute z-10 bottom-0 right-0 w-4 h-4" />
-                            </>
-                        )}
-                        {currentIndex === index && (
-                            <Image
-                                className="object-cover"
-                                src={item.image.fileUrl}
-                                alt="Картинка карточки"
-                                sizes="(max-width:)"
-                                fill
-                            />
-                        )}
-                        {currentIndex < index && (
-                            <Image
-                                className="object-cover"
-                                src={item.image.fileUrl}
-                                alt="Картинка карточки"
-                                sizes="(max-width:)"
-                                fill
-                            />
-                        )}
-                    </div>
-                ) : (
-                    <div
-                        className="relative w-12/12 md:w-10/12 lg:w-8/12 aspect-square overflow-hidden border rounded"
-                        key={item.id + item.name}
-                    >
-                        <div
-                            key={item.id + item.name}
-                            className="flex w-full h-full items-center justify-center"
-                        >
-                            <UserIcon className="w-1/2 h-1/2" />
-                        </div>
-                    </div>
-                ),
-            )}
-        </div>
+    const currentIndex = items.findIndex(
+        dung => dung.id === currentItem?.id && dung.name === currentItem.name,
     );
+
+    const getCard = (card: Monster | Dungeon, index: number) => {
+        if (!card.image) {
+            return (
+                <div className="relative w-12/12 md:w-10/12 lg:w-8/12 aspect-square overflow-hidden rounded">
+                    <UserIcon className="w-1/2 h-1/2" />
+                </div>
+            );
+        }
+
+        if (currentIndex > index) {
+            return (
+                <div className="relative w-12/12 md:w-10/12 lg:w-8/12 aspect-square overflow-hidden rounded">
+                    <CardWithCheck item={card} />
+                </div>
+            );
+        }
+
+        if (currentIndex === index) {
+            return (
+                <div className="relative w-12/12 md:w-10/12 lg:w-8/12 aspect-square overflow-hidden rounded">
+                    <Image
+                        className="object-cover"
+                        src={card.image.fileUrl}
+                        alt="Картинка карточки"
+                        sizes="(max-width:)"
+                        fill
+                    />
+                </div>
+            );
+        }
+
+        if (currentIndex < index) {
+            return (
+                <div className="relative w-full md:w-10/12 lg:w-8/12 aspect-square overflow-hidden rounded">
+                    <Image
+                        className="object-cover brightness-50"
+                        src={card.image.fileUrl}
+                        alt="Картинка карточки"
+                        sizes="(max-width:)"
+                        fill
+                    />
+                </div>
+            );
+        }
+    };
+
+    return items.map(getCard);
 };
