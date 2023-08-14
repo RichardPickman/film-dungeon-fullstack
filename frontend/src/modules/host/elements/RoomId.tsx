0503bebd4b8ca38cac82c86073925a8515495212
RichardPickman <drezzerock@gmail.com>
2023-08-14 21:31:18 +0200

    change copy button to single label

--- frontend/src/modules/host/elements/RoomId.tsx
+++ frontend/src/modules/host/elements/RoomId.tsx
@@ -1,33 +1,17 @@
 'use client';
 
-import { Button } from '@/components/ui/button';
 import { Label } from '@/components/ui/label';
-import { toast } from '@/components/ui/use-toast';
 
 const address = process.env.NEXT_PUBLIC_UI_URL + '/game/';
 
 export const RoomId = ({ roomId }: { roomId: string }) => {
     const link = address + roomId;
 
-    const copyToClipboard = () => {
-        navigator.clipboard.writeText(link);
-
-        toast({
-            title: 'Address copied to clipboard',
-        });
-    };
-
     return (
-        <div className="flex w-1/2 justify-center gap-2 p-2">
-            <Label className="flex w-1/2 items-center justify-start whitespace-nowrap p-2 rounded bg-accent overflow-auto">
+        <div className="flex w-full justify-center gap-2 p-2">
+            <Label className="flex w-1/2 items-center justify-center whitespace-nowrap p-2 rounded bg-accent overflow-auto">
                 {link}
             </Label>
-            <Button
-                className="w-fit whitespace-nowrap"
-                onClick={copyToClipboard}
-            >
-                Скопировать
-            </Button>
         </div>
     );
 };
